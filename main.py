71ff078752d1a776a66502a5dd9ab97fde1a3121
ronyronen <rony.ronen@gmail.com>
2023-04-02 09:08:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from Bereshit import Bereshit
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # b = Bereshit()
-    # b.land()
-    # b = Bereshit()
-    # b.land_traj()
+    b = Bereshit()
+    b.land()
+    b = Bereshit()
+    b.land_traj()
     b = Bereshit()
     b.land_pid()
